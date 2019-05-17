39b92d87dc5b577d0f0ac174ab9f95969bab03d5
ferrerofranco <ferrero.franco.07@gmail.com>
2019-05-17 15:45:51 -0300

    added comments, removed testing code

--- datasplit.py
+++ datasplit.py
@@ -6,9 +6,15 @@ def split(dataframe,train_pct):
     this method splits the dataset in 6, 3 with train val test features and 3 with train val test labels
     returns x_train,x_val,x_test,y_train,y_val,y_test
     '''
+#    we get the train percentage and divide the remaining between validation and test equally
+#    this work by setting % where to cut the data, for example, if we were to receive 0.6
+#    we will set cuts at 0.6 and 0.8. So 0 to 0.6 is train, 0.6 to 0.8 is val and 0.8 to 1 is test
     TRAIN_PCT = train_pct
     OTHER_PCT = 1-((1-TRAIN_PCT)/2)
-
+    
+#    we want to keep the class percentages by dataset division. (for example if happy faces represent a 20% of our whole dataset
+#    we would like to have 20% of train be happy faces, 20% of validation and 20% of test)
+#    one way to do that is to split them up, divide them and then join them back again. So we are first going to split by emotion
     neutral_df =  dataframe[dataframe['new_emotion_cd']==0]
     happy_df =  dataframe[dataframe['new_emotion_cd']==1]
     surprise_df =  dataframe[dataframe['new_emotion_cd']==2]
@@ -18,6 +24,7 @@ def split(dataframe,train_pct):
     fear_df =  dataframe[dataframe['new_emotion_cd']==6]
     contempt_df =  dataframe[dataframe['new_emotion_cd']==7]
     
+#    now we will split each emotion in train/val/test according to our desired percentages
     neutral_train, neutral_val, neutral_test = np.split(neutral_df.sample(frac=1), [int(TRAIN_PCT*len(neutral_df)), int(OTHER_PCT*len(neutral_df))])
     happy_train, happy_val, happy_test = np.split(happy_df.sample(frac=1), [int(TRAIN_PCT*len(happy_df)), int(OTHER_PCT*len(happy_df))])
     surprise_train, surprise_val, surprise_test = np.split(surprise_df.sample(frac=1), [int(TRAIN_PCT*len(surprise_df)), int(OTHER_PCT*len(surprise_df))])
@@ -27,6 +34,7 @@ def split(dataframe,train_pct):
     fear_train, fear_val, fear_test = np.split(fear_df.sample(frac=1), [int(TRAIN_PCT*len(fear_df)), int(OTHER_PCT*len(fear_df))])
     contempt_train, contempt_val, contempt_test = np.split(contempt_df.sample(frac=1), [int(TRAIN_PCT*len(contempt_df)), int(OTHER_PCT*len(contempt_df))])
     
+#    we don't want 24 dataframes, so we will now join all train,val and test emotions
     train_frames = [neutral_train,happy_train,surprise_train,sad_train,anger_train,disgust_train,fear_train,contempt_train]
     val_frames = [neutral_val,happy_val,surprise_val,sad_val,anger_val,disgust_val,fear_val,contempt_val]
     test_frames = [neutral_test,happy_test,surprise_test,sad_test,anger_test,disgust_test,fear_test,contempt_test]
@@ -35,10 +43,12 @@ def split(dataframe,train_pct):
     val = pd.concat(val_frames)
     test = pd.concat(test_frames)
     
+#    a simple shuffle so we get all emotions randomly mixed
     train = train.sample(frac=1)
     val = val.sample(frac=1)
     test = test.sample(frac=1)
     
+#    lastly we divide into features and lables
     x_train = train['pixels'] 
     x_val = val['pixels']
     x_test = test['pixels']
@@ -48,44 +58,3 @@ def split(dataframe,train_pct):
     y_test = test['new_emotion_cd']
 
     return x_train,x_val,x_test,y_train,y_val,y_test
-
-#fer_dir = 'C:/Users/franco.ferrero/Documents/Datasets/fer2013-OZ.csv'
-#TRAIN_PCT = 0.6
-#
-#
-#fer_dataset = pd.read_csv(fer_dir)
-#fer_dataset.drop(['old_emotion_cd','odl_emotion_desc','old_usage','new_emotion_desc'],axis=1,inplace=True)
-##print('Old DS Head\n',fer_dataset.head(),'\n')
-##print('Old DS Columns\n',fer_dataset.columns,'\n')
-##print('Old emotion count\n', fer_dataset['old_emotion_cd'].value_counts())
-##print('New emotion count\n', fer_dataset['new_emotion_cd'].value_counts())
-##new_fer_ds= fer_dataset[['pixels','new_emotion_cd']]
-##print('New DS Head\n',new_fer_ds.head(),'\n')
-##print('New DS Columns\n',new_fer_ds.columns,'\n')
-#
-#
-#
-#
-##print('neutral {}\nhappy {}\nsurprise {}\nsadness {}\nanger {}\ndisgust {}\nfear {}\ncontempt {}\n'.format(len(neutral_df)
-##,len(happy_df)
-##,len(surprise_df)
-##,len(sad_df)
-##,len(anger_df)
-##,len(disgust_df)
-##,len(fear_df)
-##,len(contempt_df)))
-#
-#
-#x_train,x_val,x_test,y_train,y_val,y_test = split(fer_dataset,TRAIN_PCT)
-#
-#print('x_train shape {}, x_val shape {}, x_test shape {}, y_train shape {}, y_val shape {}, y_test shape {}'.format(x_train.shape
-#      ,x_val.shape,x_test.shape,y_train.shape,y_val.shape,y_test.shape))
-#
-#
-##from sklearn import model_selection as ms
-##
-##df_train, df_val_test = ms.train_test_split(fer_dir,train_size=0.6,shuffle=True)#,stratify=fer_dir['new_emotion_cd'])
-##df_val, df_test = ms.train_test_split(df_val_test, test_size=0.5,shuffle=True)#,stratify=df_val_test['new_emotion_cd'])
-##
-##print(df_test)
-##print('orig shape {}, train shape {}, val shape{}, test shape{}'.format(neutral_df.shape,neutral_train.shape,neutral_val.shape,neutral_test.shape))
