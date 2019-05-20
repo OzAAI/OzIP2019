fdea65e31b8f7297bcf7ddfe062b076e3228b704
ferrerofranco <ferrero.franco.07@gmail.com>
2019-05-20 13:51:24 -0300

    model now creates and returns a model_1. The idea is to perhapshave multiple models to test

--- model.py
+++ model.py
@@ -1,2 +1,65 @@
-def get_model(param):
-    return None
+from keras import regularizers
+from keras import layers
+from keras import models
+
+def define_model_1(l2_regularization,input_shape,num_classes):
+    # model parameters
+    regularization = regularizers.l2(l2_regularization)
+     
+    # base
+    img_input = layers.Input(input_shape)
+    x = layers.Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization, use_bias=False)(img_input)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+     
+    # module 1
+    residual = layers.Conv2D(16, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = layers.BatchNormalization()(residual)
+    x = layers.SeparableConv2D(16, (3, 3), padding='same', kernel_regularizer=regularization, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.SeparableConv2D(16, (3, 3), padding='same', kernel_regularizer=regularization, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+     
+    # module 2
+    residual = layers.Conv2D(32, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
+    residual = layers.BatchNormalization()(residual)
+    x = layers.SeparableConv2D(32, (3, 3), padding='same', kernel_regularizer=regularization, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.SeparableConv2D(32, (3, 3), padding='same', kernel_regularizer=regularization, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+     
+    # module 3
+    residual = layers.Conv2D(64, (1, 1), strides=(2, 2),padding='same', use_bias=False)(x)
+    residual = layers.BatchNormalization()(residual)
+    x = layers.SeparableConv2D(64, (3, 3), padding='same',kernel_regularizer=regularization,use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.SeparableConv2D(64, (3, 3), padding='same',kernel_regularizer=regularization,use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+     
+    # module 4
+    residual = layers.Conv2D(128, (1, 1), strides=(2, 2),padding='same', use_bias=False)(x)
+    residual = layers.BatchNormalization()(residual)
+    x = layers.SeparableConv2D(128, (3, 3), padding='same',kernel_regularizer=regularization,use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.SeparableConv2D(128, (3, 3), padding='same',kernel_regularizer=regularization,use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+    x = layers.Conv2D(num_classes, (3, 3), padding='same')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    output = layers.Activation('softmax',name='predictions')(x)
+     
+    return models.Model(img_input, output)
